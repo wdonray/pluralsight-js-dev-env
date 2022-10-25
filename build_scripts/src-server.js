4d4cebcecf1b41f92c41b4003121afcded5a10f1
Donray Williams <donrayxwilliams@gmail.com>
2022-10-25 12:51:49 -0500

    bundled code using webpack

--- build_scripts/src-server.js
+++ build_scripts/src-server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { join } from "path";
 import open from "open";
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
-
 const app = express();
+const compiler = webpack(config);
+
+// Tell express to use my webpack configuration
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
 
 // Any reference to the root will respond with sending the html file
 app.get("/", (req, res) => {
